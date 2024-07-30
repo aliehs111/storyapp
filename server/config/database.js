f1b41e87c3a253015d88003c575247f184ca0a8f
aliehs111 <128503077+aliehs111@users.noreply.github.com>
2024-07-30 16:34:46 -0400

    changes for db deployment

--- server/config/database.js
+++ server/config/database.js
@@ -4,9 +4,13 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-});
+const isProduction = process.env.NODE_ENV === 'production';
+
+const sequelize = isProduction
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+    });
 
 module.exports = sequelize;
