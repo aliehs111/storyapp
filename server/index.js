f1e75b8295c90bb1bb2ae7f2edf2e50e77fdc9ca
aliehs111 <128503077+aliehs111@users.noreply.github.com>
2024-07-30 17:21:21 -0400

    fixes db setup

--- server/index.js
+++ server/index.js
@@ -2,13 +2,12 @@ const dotenv = require('dotenv');
 dotenv.config(); // Ensure this is at the very top
 const express = require('express');
 const session = require('express-session');
-const passport = require('./config/passport');
-const mysql = require('mysql2');
 const cors = require('cors');
+const passport = require('./config/passport');
+const sequelize = require('./config/database'); // Ensure this is your Sequelize instance
 const userRoutes = require('./routes/userRoutes'); // Correct import of userRoutes
 const videoRoutes = require('./routes/videoRoutes'); // Correct import of videoRoutes
 const authRoutes = require('./routes/authRoutes'); // Correct import of authRoutes
-require('./config/passport'); // Ensure this is required to initialize passport strategies
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -18,24 +17,11 @@ app.use(express.json());
 
 // CORS configuration
 app.use(cors({
-  origin: 'http://localhost:3000', // Adjust this to your frontend URL
+  origin: process.env.CORS_ORIGIN || 'http://localhost:3000', // Adjust this to your frontend URL
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   allowedHeaders: 'Content-Type, Authorization',
 }));
-
-// MySQL connection
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
-
-db.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to database');
-});
 
 // Express session
 app.use(session({
@@ -57,12 +43,16 @@ app.use('/api/videos', videoRoutes);
 // Use userRoutes
 app.use('/api/users', userRoutes);
 
-
 // Define a route for the root URL
 app.get('/', (req, res) => {
   res.send('Welcome to the Story App!');
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+// Connect to the database and start the server
+sequelize.sync().then(() => {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}).catch((err) => {
+  console.error('Unable to connect to the database:', err);
 });
